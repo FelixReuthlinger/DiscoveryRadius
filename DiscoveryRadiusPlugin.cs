d5643fe9458bd4810008d216390922ca60cad98b
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2023-10-12 23:01:28 +0200

    1.0.2

--- DiscoveryRadiusPlugin.cs
+++ DiscoveryRadiusPlugin.cs
@@ -14,7 +14,7 @@ namespace DiscoveryRadius;
 public class DiscoveryRadiusPlugin : BaseUnityPlugin
 {
     public const string ModName = "DiscoveryRadius";
-    public const string ModVersion = "1.0.1";
+    public const string ModVersion = "1.0.2";
     private const string ModAuthor = "FixItFelix";
     private const string ModGuid = ModAuthor + "." + ModName;
 
